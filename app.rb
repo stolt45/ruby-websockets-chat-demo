b5dbbb807700b3ff90f807299e600bdb699fcac4
Christopher Stolt <stolt45@gmail.com>
2014-11-18 15:55:29 -0800

    removing ssl for default

--- app.rb
+++ app.rb
@@ -10,7 +10,8 @@ module ChatDemo
 
     get "/assets/js/application.js" do
       content_type :js
-      @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
+      @scheme = "ws://"
+      #@scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
       erb :"application.js"
     end
   end
